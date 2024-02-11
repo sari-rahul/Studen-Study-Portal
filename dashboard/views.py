25901349aa4ac46c78404efa6f1ef6c4195eb3e4
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-02-11 13:36:32 +0000

    Add note page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Note
 # Create your views here.
 
 def home (request):
     return render (request,"dashboard/home.html")
+
+def notes (request):
+    note = Note.object.filter(user=request.user)
+    context ={'notes':notes}
+    return render (request,"dashboard/notespage.html",context)
