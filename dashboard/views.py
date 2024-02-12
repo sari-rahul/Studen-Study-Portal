31f691a0ab9de4cad5128b54793e9ab3804338e0
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-02-12 09:49:34 +0000

    Complete Create Notes function

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from . forms import *
 # Create your views here.
 
@@ -6,7 +7,17 @@ def home (request):
     return render (request,"dashboard/home.html")
 
 def notes (request):
-    form = Notesform()
+    if request.method == "POST":
+        form = Notesform(request.POST)
+        if form.is_valid():
+            notes = Note(
+                user=request.user,title=request.POST['title'],
+                description=request.POST['description'])
+            notes.save()
+        messages.success(request,f"Notes added successfully from {request.user.username}")
+    else:
+        form = Notesform()
+
     notes = Note.objects.filter(user=request.user)
     context ={'notes':notes,'form':form}
     return render (request,"dashboard/notes.html",context)
