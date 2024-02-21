60994866f98914e099b1042bd5bbcd964e0b42b8
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-02-21 14:23:39 +0000

    Add form tests

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -17,10 +17,13 @@ class Homeworkform(forms.ModelForm):
         widgets = {'due_date':DateInput()}
         fields = ['subject','title','description','due_date','is_finished']
 
-class Commonform(forms.Form):
-    search_text = forms.CharField(max_length=100,label='Enter your search here')
 
 class Todoform(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title','is_completed']
+
+
+class Commonform(forms.Form):
+    search_text = forms.CharField(max_length=100,label='Enter your search here')
+
